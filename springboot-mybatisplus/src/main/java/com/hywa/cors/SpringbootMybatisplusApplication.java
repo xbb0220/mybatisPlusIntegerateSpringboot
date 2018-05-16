f5f38e69f933a5c7fa36b8fd3a6e75b2a96a5bca
徐彬彬 <xbb0220@163.com>
2018-05-16 16:55:52 +0800

    test commit

--- springboot-mybatisplus/src/main/java/com/hywa/cors/SpringbootMybatisplusApplication.java
+++ springboot-mybatisplus/src/main/java/com/hywa/cors/SpringbootMybatisplusApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootMybatisplusApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(SpringbootMybatisplusApplication.class, args);
-		
 	}
 
 }
