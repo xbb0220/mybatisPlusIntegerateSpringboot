c78afe4fba54aa8e445d33c84261cc5683957dc5
徐彬彬 <xbb0220@163.com>
2018-05-16 16:32:55 +0800

    测试git提交

--- springboot-mybatisplus/src/main/java/com/hywa/cors/SpringbootMybatisplusApplication.java
+++ springboot-mybatisplus/src/main/java/com/hywa/cors/SpringbootMybatisplusApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootMybatisplusApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringbootMybatisplusApplication.class, args);
+		
 	}
 
 }
