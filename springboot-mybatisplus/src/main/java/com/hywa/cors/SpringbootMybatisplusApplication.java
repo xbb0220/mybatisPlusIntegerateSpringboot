afba49309e7dd407d4ef7a6b544e75e0cd30e6e8
徐彬彬 <xbb0220@163.com>
2018-05-17 09:02:49 +0800

    添加事物管理

--- springboot-mybatisplus/src/main/java/com/hywa/cors/SpringbootMybatisplusApplication.java
+++ springboot-mybatisplus/src/main/java/com/hywa/cors/SpringbootMybatisplusApplication.java
@@ -2,8 +2,10 @@ package com.hywa.cors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement  
 public class SpringbootMybatisplusApplication {
 
 	public static void main(String[] args) {
